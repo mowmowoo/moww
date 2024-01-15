eb8c0358469b32cd2388a2fcdf2064b1c0c4d76b
SLA-SGD <technologysgd@gmail.com>
2024-01-15 17:05:41 +0800

    01152024barawat

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React from "react";
 
-export const intro = () => {
+export const Intro = () => {
   return (
     <section>
       <div className="flex items-center justify-center">
@@ -9,7 +9,18 @@ export const intro = () => {
           <Image
             src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
             alt="Ricardo portrait"
+            width="192"
+            height="192"
+            quality="95"
+            priority={true}
+            className="h-24 w-24 rounded-full 
+              border-white border-[0.35rem] shadow-xl object-cover
+            "
           />
+
+          <span className="text-4xl">
+            👋
+          </span>
         </div>
       </div>
     </section>
