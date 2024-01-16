a174afa8d6e9359c059e8654293152be1198b8ea
SLA User <sla3dgc@gmail.com>
2024-01-16 17:17:02 +0800

    16012024evert

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,8 +4,8 @@ import React from "react";
 export const Intro = () => {
   return (
     <section>
-      <div className="flex items-center justify-center">
-        <div>
+      <div className=" flex items-center justify-center">
+        <div className="relative">
           <Image
             src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
             alt="Ricardo portrait"
@@ -18,7 +18,14 @@ export const Intro = () => {
             "
           />
 
-          <span className="text-4xl">
+          <span
+            className="
+          absolute 
+          
+          bottom-0 right-0 text-4xl
+      
+          "
+          >
             👋
           </span>
         </div>
