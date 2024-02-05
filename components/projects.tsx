e8b8b98d27c0b3f81b7385f263b9a44514a0eb75
SLA User <sla3dgc@gmail.com>
2024-02-05 17:59:18 +0800

    evert522024

--- components/projects.tsx
+++ components/projects.tsx
@@ -18,30 +18,57 @@ export default function Projects() {
   );
 }
 
-type ProjectProps = (typeof projectsData)[number];
+type ProjectProps = typeof projectsData[number];
 
 function Project({ title, description, tags, imageUrl }: ProjectProps) {
   return (
     <section
       className="bg-gray-100 max-w-[42rem]
-        border border-black/5 overflow-hidden sm:pr-8 relative
-"
+        border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]
+        mb-3 sm:mb-8 group even:pl-8 hover:bg-gray-200 transition 
+        "
     >
-      <h3 className="text-2xl font-semibold">{title}</h3>
-      <p className="leading-relaxed mt-2 text-gray-700">{description}</p>
-      <ul className="flex flex-wrap mt-4 gap-2">
-        {tags.map((tag, index) => (
-          <li
-            className="bg-black/[0.7] text-[0.7rem] text-white px-3 py-1 
+      <div
+        className="pt-4 pb-7 px-5 sm:pl-10  sm:pt-10 sm: pr-2 sm:max-w-[50%]
+                      flex flex-col h-full group-even:ml-[18rem]
+                      "
+      >
+        <h3 className="text-2xl font-semibold">{title}</h3>
+        <p className="leading-relaxed mt-2 text-gray-700 pl-50">{description}</p>
+        <ul
+          className="flex flex-wrap mt-4 gap-2 
+                        sm:mt-auto"
+        >
+          {tags.map((tag, index) => (
+            <li
+              className="bg-black/[0.7] text-[0.7rem] text-white px-3 py-1 
                         uppercase tracking-wider rounded-full
                     "
-            key={index}
-          >
-            {tag}
-          </li>
-        ))}
-      </ul>
-      <Image className="absolute top-8 -right-40" src={imageUrl} alt="Project I worked on" quality={95} />
+              key={index}
+            >
+              {tag}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <Image
+        className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+        group-even:right-[initial] group-even:-left-40
+        transition
+        group-hover:-translate-x-3
+        group-hover:translate-y-3
+        group-hover:-rotate-2
+
+        group-even:group-hover:translate-x-3
+        group-even:group-hover:translate-y-3
+        group-even:group-hover:rotate-2
+
+        group-hover:scale-[1.04]
+        "
+        src={imageUrl}
+        alt="Project I worked on"
+        quality={95}
+      />
     </section>
   );
 }
