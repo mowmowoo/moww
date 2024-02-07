261a40cff105f1c376ee2b6d3ccaf3c9e5d1612f
SLA User <sla3dgc@gmail.com>
2024-02-07 17:56:33 +0800

    evert722024

--- components/project.tsx
+++ components/project.tsx
@@ -1,57 +1,71 @@
-"use client"
+"use client";
 
 import { projectsData } from "@/lib/data";
-import { motion, useScroll } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
 import { useRef } from "react";
 
 type ProjectProps = (typeof projectsData)[number];
 
-export default function Project({ title, description, tags, imageUrl }: ProjectProps) {
-  const ref = useRef<HTMLElement>(null)
+export default function Project({
+  title,
+  description,
+  tags,
+  imageUrl,
+}: ProjectProps) {
+  const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ["0 1", "1.33 1"],
   });
 
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1])
+  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1])
+
+
   return (
-    <motion.section ref={ref}
-      className="bg-gray-100 max-w-[42rem]
-        border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]
-        mb-3 sm:mb-8 group even:pl-8 hover:bg-gray-200 transition 
-        "
-        style={{
-            scale: scrollYProgress,
-            opacity: scrollYProgress,
-        }}
+    <motion.div
+      className="mb-3 sm:mb-8 group last:mb-0"
+      ref={ref}
+      style={{
+        scale: scaleProgress,
+        opacity: opacityProgress
+        ,
+      }}
     >
-      <div
-        className="pt-4 pb-7 px-5 sm:pl-10  sm:pt-10 sm: pr-2 sm:max-w-[50%]
+      <section
+        className="bg-gray-100 max-w-[42rem]
+        border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]
+        even:pl-8 hover:bg-gray-200 transition 
+        "
+      >
+        <div
+          className="pt-4 pb-7 px-5 sm:pl-10  sm:pt-10 sm: pr-2 sm:max-w-[50%]
                       flex flex-col h-full group-even:ml-[18rem]
                       "
-      >
-        <h3 className="text-2xl font-semibold">{title}</h3>
-        <p className="leading-relaxed mt-2 text-gray-700 pl-50">
-          {description}
-        </p>
-        <ul
-          className="flex flex-wrap mt-4 gap-2 
-                        sm:mt-auto"
         >
-          {tags.map((tag, index) => (
-            <li
-              className="bg-black/[0.7] text-[0.7rem] text-white px-3 py-1 
+          <h3 className="text-2xl font-semibold">{title}</h3>
+          <p className="leading-relaxed mt-2 text-gray-700 pl-50">
+            {description}
+          </p>
+          <ul
+            className="flex flex-wrap mt-4 gap-2 
+                        sm:mt-auto"
+          >
+            {tags.map((tag, index) => (
+              <li
+                className="bg-black/[0.7] text-[0.7rem] text-white px-3 py-1 
                         uppercase tracking-wider rounded-full
                     "
-              key={index}
-            >
-              {tag}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <Image
-        className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+                key={index}
+              >
+                {tag}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <Image
+          className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
         group-even:right-[initial] group-even:-left-40
         transition
         group-hover:-translate-x-3
@@ -64,10 +78,11 @@ export default function Project({ title, description, tags, imageUrl }: ProjectP
 
         group-hover:scale-[1.04]
         "
-        src={imageUrl}
-        alt="Project I worked on"
-        quality={95}
-      />
-    </motion.section>
+          src={imageUrl}
+          alt="Project I worked on"
+          quality={95}
+        />
+      </section>
+    </motion.div>
   );
 }
