4769429d4af774ba194d2564cf393d5bf6aa5f67
jim <jimhchv@gmail.com>
2024-02-12 23:00:15 +0800

    jim122024

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,15 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { links } from "@/lib/data";
 import Link from "next/link";
+import clsx from "clsx"
 
 export const Header = () => {
+  const [activeSection, setActiveSection ] =
+  useState('Home')
+
   return (
     <header className="z-999 relative">
       <motion.div
