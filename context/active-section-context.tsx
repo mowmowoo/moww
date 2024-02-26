f9514cabc8602d36e13e147d9255edfe8a119ff1
jim <jimhchv@gmail.com>
2024-02-26 23:10:07 +0800

    jim24022024

--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -1,24 +1,28 @@
 import React, { useState, createContext } from "react";
 import { links } from "@/lib/data";
 
-type SectionName = typeof links[number]["name"];
+type SectionName = typeof links[number]["name"];     //get link name from data 
 
 type ActiveSectionContextProviderProps = {
   children: React.ReactNode;
 };
 
-type ActiveSectionContextType = {
-  
-}
+type ActiveSectionContextType = {              //useState
+  activeSection: SectionName;
+  setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
 
-const ActiveSectionContext = createContext(null);
+};
+
+const ActiveSectionContext = createContext<ActiveSectionContextType | null>(null);
 
 export default function ActiveSectionContextProvider({
   children,
 }: ActiveSectionContextProviderProps) {
   const [activeSection, setActiveSection] = useState<SectionName>("Home");
 
-  return (<ActiveSectionContext.Provider value={{activeSection, setActiveSection}}>
-    {children}
-  </ActiveSectionContext.Provider>)
+  return (
+    <ActiveSectionContext.Provider value={{ activeSection, setActiveSection }}>
+      {children}
+    </ActiveSectionContext.Provider>
+  );
 }
